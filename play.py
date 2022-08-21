66ceb6be10dd295ecae2bc5c42504e5c2aeaaa45
naszifnaaz <naszifnaaz@gmail.com>
2022-08-22 00:18:52 +0530

    ci: rendered-bird-sprite-along-with-physics

--- play.py
+++ play.py
@@ -17,6 +17,11 @@ background_scene = pygame.transform.scale(background_scene, (width, height))
 floor_scene = pygame.image.load('assets/scene/floor.png').convert()
 floor_scene = pygame.transform.scale2x(floor_scene)
 
+# Loading bird sprites
+bird_sprite = pygame.image.load('assets/sprites/bird_mid.png').convert()
+bird_sprite = pygame.transform.scale2x(bird_sprite)
+bird_hitbox = bird_sprite.get_rect(center=(100, 400))
+
 
 # Drawing dynamic floor
 def draw_floor():
@@ -31,8 +36,20 @@ while run:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                bird_velocity = 0
+                bird_velocity -= 10
+
     # dynamic floor
     win.blit(background_scene, (0, 0))
+
+    # implementing bird physics
+    bird_velocity += gravity
+    bird_hitbox.centery += bird_velocity
+
+    win.blit(bird_sprite, bird_hitbox)
     dynamic_floor -= 1
     draw_floor()
     if dynamic_floor <= -500:
--- setup/constants.py
+++ setup/constants.py
@@ -1,10 +1,14 @@
 # Game setup
 
 # Window constants
-width = 576
+width = 524
 height = 800
 fps = 120
 
 # Gameplay constants
 run = True
+
+# Game Physics
 dynamic_floor = 0
+gravity = 0.25
+bird_velocity = 0
